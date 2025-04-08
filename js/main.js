22a7506efc9ecbdccebb0228a816aadde0c6071e
Ricardo MR <ricardomr.23@hotmail.com>
2025-04-08 13:05:48 -0600

    Se modifica el js

--- js/main.js
+++ js/main.js
@@ -34,8 +34,36 @@ const handleEventList = (e) => {
 
   // listas.item(0).before(element2)
   // listas.item(0).prepend(element)
-  listas.item(0).append(element);
-  listas.item(0).after(element2);
+  // listas.item(0).append(element);
+  // listas.item(0).after(element2);
+
+  // listas.item(1).insertAdjacentElement("afterbegin", element)
+  // listas.item(1).insertAdjacentElement("beforeend", element2)
+
+  listas
+    .item(1)
+    .insertAdjacentHTML(
+      "beforebegin",
+      `<li class="list-group-item">Before begin item</li>`
+    );
+  listas
+    .item(1)
+    .insertAdjacentHTML(
+      "afterend",
+      `<li class="list-group-item">After end item</li>`
+    );
+  listas
+    .item(1)
+    .insertAdjacentHTML(
+      "afterbegin",
+      `<li class="list-group-item">After begin item</li>`
+    );
+  listas
+    .item(1)
+    .insertAdjacentHTML(
+      "beforeend",
+      `<li class="list-group-item">Before end item</li>`
+    );
 };
 
 btnMostrar2.addEventListener("click", handleEventList);
